5de467a73277c9b6df9bbd1990c916ed147259d3
S-AitAli <saraaitali1997@gmail.com>
2025-03-17 08:55:55 -0400

    Final version of the App

---
+++ FinalProjectSA/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace FinalProjectSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUploadFile = new System.Windows.Forms.Button();
+            this.txtRowsAffected = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRowsProcessed = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtMinRainfall = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAvgRainfall = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtMaxRainfall = new System.Windows.Forms.TextBox();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.txtInputFile = new System.Windows.Forms.TextBox();
+            this.txtOutputFile = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.label1.Location = new System.Drawing.Point(146, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Rain Fall Processor";
+            // 
+            // btnUploadFile
+            // 
+            this.btnUploadFile.Location = new System.Drawing.Point(172, 120);
+            this.btnUploadFile.Name = "btnUploadFile";
+            this.btnUploadFile.Size = new System.Drawing.Size(94, 32);
+            this.btnUploadFile.TabIndex = 1;
+            this.btnUploadFile.Text = "Process";
+            this.btnUploadFile.UseVisualStyleBackColor = true;
+            this.btnUploadFile.Click += new System.EventHandler(this.btnUploadFile_Click);
+            // 
+            // txtRowsAffected
+            // 
+            this.txtRowsAffected.Location = new System.Drawing.Point(235, 162);
+            this.txtRowsAffected.Name = "txtRowsAffected";
+            this.txtRowsAffected.Size = new System.Drawing.Size(84, 20);
+            this.txtRowsAffected.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(132, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rows Affected:";
+            // 
+            // txtRowsProcessed
+            // 
+            this.txtRowsProcessed.Location = new System.Drawing.Point(156, 28);
+            this.txtRowsProcessed.Name = "txtRowsProcessed";
+            this.txtRowsProcessed.Size = new System.Drawing.Size(84, 20);
+            this.txtRowsProcessed.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(45, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rows processed:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Minimum Rainfall:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(46, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Maximum Rainfall:";
+            // 
+            // txtMinRainfall
+            // 
+            this.txtMinRainfall.Location = new System.Drawing.Point(156, 70);
+            this.txtMinRainfall.Name = "txtMinRainfall";
+            this.txtMinRainfall.Size = new System.Drawing.Size(84, 20);
+            this.txtMinRainfall.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtAvgRainfall);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtMaxRainfall);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtMinRainfall);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtRowsProcessed);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(87, 205);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(316, 262);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Rainfall Statistics";
+            // 
+            // txtAvgRainfall
+            // 
+            this.txtAvgRainfall.Location = new System.Drawing.Point(156, 183);
+            this.txtAvgRainfall.Name = "txtAvgRainfall";
+            this.txtAvgRainfall.Size = new System.Drawing.Size(84, 20);
+            this.txtAvgRainfall.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(46, 190);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Average Rainfall:";
+            // 
+            // txtMaxRainfall
+            // 
+            this.txtMaxRainfall.Location = new System.Drawing.Point(156, 119);
+            this.txtMaxRainfall.Name = "txtMaxRainfall";
+            this.txtMaxRainfall.Size = new System.Drawing.Size(84, 20);
+            this.txtMaxRainfall.TabIndex = 9;
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(349, 75);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(91, 23);
+            this.btnChooseFile.TabIndex = 11;
+            this.btnChooseFile.Text = "Import ";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // txtInputFile
+            // 
+            this.txtInputFile.Location = new System.Drawing.Point(98, 78);
+            this.txtInputFile.Name = "txtInputFile";
+            this.txtInputFile.Size = new System.Drawing.Size(232, 20);
+            this.txtInputFile.TabIndex = 12;
+            // 
+            // txtOutputFile
+            // 
+            this.txtOutputFile.Location = new System.Drawing.Point(191, 514);
+            this.txtOutputFile.Name = "txtOutputFile";
+            this.txtOutputFile.Size = new System.Drawing.Size(249, 20);
+            this.txtOutputFile.TabIndex = 13;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(181, 473);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(102, 29);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "Save ";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(33, 85);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Select File:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(33, 521);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(140, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Processed Rows Saved To:";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(392, 571);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(80, 32);
+            this.btnClose.TabIndex = 15;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(285, 571);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(80, 32);
+            this.btnClear.TabIndex = 16;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(496, 615);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.txtOutputFile);
+            this.Controls.Add(this.txtInputFile);
+            this.Controls.Add(this.btnChooseFile);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnUploadFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRowsAffected);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Rain Fall Processor";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnUploadFile;
+        private System.Windows.Forms.TextBox txtRowsAffected;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRowsProcessed;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtMinRainfall;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtAvgRainfall;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMaxRainfall;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.TextBox txtInputFile;
+        private System.Windows.Forms.TextBox txtOutputFile;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ FinalProjectSA/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProjectSA
+{
+    /* Sara Ait Ali 
+     * Develop a windows desktop application that is capable of reading a text file, processing the rows according
+     * to my business rules, and writing a new file containing only the processed rows.
+     */
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        double[] values;
+        private void btnUploadFile_Click(object sender, EventArgs e)
+        {
+            txtInputFile.BackColor = Color.White;
+            
+
+            int rowCount = 0;
+            int goodRowCount = 0;
+            double sum = 0;
+            double min = double.MaxValue;
+            double max = double.MinValue;
+
+            List<double> tempValues = new List<double>();
+
+            string inputFilePath = txtInputFile.Text;
+            if (txtInputFile.Text == "")
+            {
+                MessageBox.Show("Please Enter a Filename!", "Empty");
+                txtInputFile.BackColor = Color.IndianRed;
+                txtInputFile.Focus();
+
+            }
+            else if (!File.Exists(inputFilePath))
+            {
+                MessageBox.Show("The input file does not exist.", "Error");
+            }
+            else
+            {
+
+                try
+                {
+
+                    using (StreamReader reader = new StreamReader(inputFilePath))
+                    {
+
+                        while (!reader.EndOfStream)
+                        {
+                            string line = reader.ReadLine();
+                            int sentinelValue = 99999;
+                            double value;
+
+                            if (double.TryParse(line, out value))
+                            {
+                                if (value == sentinelValue)
+                                {
+                                    break;
+                                }
+
+                                rowCount++;
+
+                                if (value >= 0)
+                                {
+                                    goodRowCount++;
+                                    sum += value;
+                                    min = Math.Min(min, value);
+                                    max = Math.Max(max, value);
+
+                                    tempValues.Add(value);
+
+                                }
+                            }
+                        }
+                    }
+
+                    values = tempValues.ToArray();
+
+                    double average = goodRowCount > 0 ? sum / goodRowCount : 0;
+
+                    txtRowsAffected.Text = rowCount.ToString();
+                    txtRowsProcessed.Text = goodRowCount.ToString();
+                    txtMinRainfall.Text = min.ToString();
+                    txtMaxRainfall.Text = max.ToString();
+                    txtAvgRainfall.Text = average.ToString();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred while reading the file: {ex.Message}", "Error");
+                }
+            }
+        }
+
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileChooser = new OpenFileDialog();
+
+            fileChooser.Filter = "Text Files (*.txt)|*.txt";
+            fileChooser.Title = "Select an input file";
+
+            if (fileChooser.ShowDialog() == DialogResult.OK)
+            {
+                
+                txtInputFile.Text = fileChooser.FileName;
+            }
+        
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            
+
+            SaveFileDialog saveFile = new SaveFileDialog();
+
+            saveFile.Filter = "Text Files (*.txt)|*.txt";
+            saveFile.Title = "Select an output file";
+
+            if (saveFile.ShowDialog() == DialogResult.OK)
+            {
+                
+                string outputFilePath = saveFile.FileName;
+
+                try
+                {
+                    using (StreamWriter writer = new StreamWriter(outputFilePath))
+                    {
+                        foreach (double value in values)
+                        {
+                            writer.WriteLine(value);
+                        }
+                    }
+
+                    txtOutputFile.Text = outputFilePath;
+                    MessageBox.Show("Your File was successfully created and saved.", "Success");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred while writing to the file: {ex.Message}", "Error");
+                }
+            }
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtInputFile.Text = "";
+            txtRowsAffected.Text = "";
+            txtRowsProcessed.Text = "";
+            txtMinRainfall.Text = "";
+            txtMaxRainfall.Text = "";
+            txtAvgRainfall.Text = "";
+            txtOutputFile.Text = "";
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+        }
+
+      
+  
